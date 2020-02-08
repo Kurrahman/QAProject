02be232f677331048429fb26419744d0b55cc878
Taufikurrahman <Anwar>
2020-02-08 16:08:26 +0700

    Initial commit

---
+++ src/test/java/future/phase2/DefinitionTestSuite.java
@@ -0,0 +1,9 @@
+package future.phase2;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/consult_dictionary/LookupADefinition.feature")
+public class DefinitionTestSuite {}
